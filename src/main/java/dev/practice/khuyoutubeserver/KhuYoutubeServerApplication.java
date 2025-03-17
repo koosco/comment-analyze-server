8e90c33acf179c980cf7547bab76437641b8fe24
koosco <rnxogud123@naver.com>
2025-03-18 01:58:09 +0900

    fix: add Server Application

---
+++ src/main/java/dev/practice/khuyoutubeserver/KhuYoutubeServerApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.khuyoutubeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhuYoutubeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhuYoutubeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/practice/khuyoutubeserver/KhuYoutubeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.khuyoutubeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuYoutubeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
